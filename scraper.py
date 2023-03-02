7b78b39409a0da2ab504edf14f82979d3e80bf54
caede1008 <79036121+caede1008@users.noreply.github.com>
2023-03-02 13:49:43 +0900

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -9,8 +9,6 @@ import openpyxl
 from tkinter import *
 from tkinter import ttk
 import requests
-from PIL import Image
-import glob
 
 # GUI設定
 root = Tk()
@@ -32,12 +30,12 @@ wkplacebox = ttk.Entry()
 wkplacebox.configure(state='normal', width=35)
 wkplacebox.place(x=110, y=60)
 
-exclusionlbl = ttk.Label(text='除外ワード')
-exclusionlbl.place(x=20, y=90)
+maxlbl = ttk.Label(text='上限件数')
+maxlbl.place(x=20, y=90)
 
-exclusionbox = ttk.Entry()
-exclusionbox.configure(state='normal', width=35)
-exclusionbox.place(x=110, y=90)
+maxbox = ttk.Entry()
+maxbox.configure(state='normal', width=10)
+maxbox.place(x=110, y=90)
 
 scrapingbutton = ttk.Button(root, text="スクレイピング実行", command=lambda:main(), width=40)
 scrapingbutton.pack(pady=130)
@@ -52,86 +50,120 @@ def main():
     # options.add_argument("--headless") # 動きを見たい場合はコメントアウトする。
     driver=webdriver.Chrome(DriverPath, options=options)
 
-    # 変数宣言
-    names = []
-    companynames =[]
-    contents = []
+    # エクセル準備
+    path = './indeed_list.xlsx'
+    wb = openpyxl.load_workbook(path)
+    ws = wb["list"]
+    exs = wb["exlist"]
+    rownumber = 2
 
     # スクレイピング準備
-    ProcessC=Process(driver)
-    ProcessC.goPage()
-    time.sleep(3)
+    url = 'https://jp.indeed.com/?from=gnav-homepage'
+    driver.get(url)
+    time.sleep(2)
+    ttlcnt = 0
 
     # キーワード設定
     keywordtxt = txtbox.get()
+    if keywordtxt.__contains__(','):
+        wklist = keywordtxt.split(',')
+        keywordtxt = ''
+        for keyword in wklist:
+            keywordtxt = keywordtxt + ' ' + keyword
     if len(keywordtxt) != 0:
         kwsearchbox = driver.find_element(By.XPATH, "//*[@id='text-input-what']")
         kwsearchbox.send_keys(keywordtxt)
 
     # 勤務地
     wkplacetext = wkplacebox.get()
+    if wkplacetext.__contains__(','):
+        wklist = wkplacetext.split(',')
+        wkplacetext = ''
+        for wktxt in wklist:
+            wkplacetext = wkplacetext + ' ' + wktxt
     if len(wkplacetext) != 0:
         wkp_searchbox = driver.find_element(By.XPATH, "//*[@id='text-input-where']")
         wkp_searchbox.send_keys(wkplacetext)
 
     # 除外ワード
-    exclusiontxt = exclusionbox.get()
-    if len(exclusiontxt) != 0:
-        kwsearchbox = driver.find_element(By.XPATH, "//*[@id='text-input-what']")
-        kwsearchbox.send_keys(" -" + exclusiontxt)
+    exstr = ''
+    firststr = exs.cell(1, 1).value
+    if firststr != None:
+        maxrow = exs.max_row + 1
+        for rn in range(1, maxrow):
+            exword = exs.cell(rn, 1).value
+            exstr = exstr + " -" + exword
+        if len(exstr) != 0:
+            kwsearchbox = driver.find_element(By.XPATH, "//*[@id='text-input-what']")
+            kwsearchbox.send_keys(exstr)
+
+    # 件数上限設定
+    maxcnt = int(maxbox.get())
 
     # 検索ボタン押下
     searchbutton = driver.find_element(By.XPATH, "//*[@id='jobsearch']/button")
     searchbutton.click()
-    time.sleep(5)
+    time.sleep(3)
 
-    # 各案件ボタン
-    buttons = []
-    buttons = driver.find_elements(By.CLASS_NAME, "css-1m4cuuf.e37uo190")
+    while True:
 
-    # Cookie同意
-    ckbutton = driver.find_element(By.CLASS_NAME, "gnav-CookiePrivacyNoticeButton")
-    ckbutton.click()
+        # 変数リセット
+        companynames = []
+        contents = []
 
-    # スクレイピング実行
-    wknames = driver.find_elements(By.CLASS_NAME, "jobTitle.css-1h4a4n5.eu4oa1w0")
-    for wkname in wknames:
-        names.append(wkname.text)
+        # 各案件ボタン
+        buttons = []
+        buttons = driver.find_elements(By.CLASS_NAME, "css-1m4cuuf.e37uo190")
 
-    wkcompanynames = driver.find_elements(By.CLASS_NAME, "companyName")
-    for wkcompanyname in wkcompanynames:
-        companynames.append(wkcompanyname.text)
+        # Cookie同意
+        ckbutton = driver.find_elements(By.CLASS_NAME, "gnav-CookiePrivacyNoticeButton")
+        if len(ckbutton) != 0:
+            ckbutton[0].click()
 
-    for button in buttons:
-        button.click()
-        time.sleep(2)
-        wkcontents = driver.find_elements(By.CLASS_NAME, "jobsearch-JobComponent-embeddedBody")
-        for wkcontent in wkcontents:
-            if len(wkcontent.text)==0:
-                contents.append('無し')
+        # スクレイピング実行
+        spcnt = 0
+        wkcompanynames = driver.find_elements(By.CLASS_NAME, "companyName")
+        for wkcompanyname in wkcompanynames:
+            companynames.append(wkcompanyname.text)
+            spcnt += 1
+            ttlcnt += 1
+            if ttlcnt > maxcnt:
+                break
+
+        for i in range(0, spcnt):
+            buttons[i].click()
+            time.sleep(2)
+            wkcontents = driver.find_elements(By.CLASS_NAME, "jobsearch-JobComponent-embeddedBody")
+            if len(wkcontents) != 0:
+                contents.append(wkcontents[0].text)
             else:
-                contents.append(wkcontent.text)
+                contents.append('無し')
 
-    # エクセル準備
-    path = './indeed_list.xlsx'
-    wb = openpyxl.load_workbook(path)
-    ws = wb["list"]
+        # Excel入力
+        idx = 0
+        for companyname in companynames:
+            ws.cell(rownumber, 1).value = companyname
+            ws.cell(rownumber, 2).value = contents[idx]
+            idx += 1
+            rownumber += 1
 
-    # Excel入力
-    rownumber = 2
-    idx = 0
-    for name in names:
-        ws.cell(rownumber, 1).value = companynames[idx]
-        ws.cell(rownumber, 2).value = name
-        ws.cell(rownumber, 3).value = contents[idx]
-        idx += 1
-        rownumber += 1
+        wb.save(path)
+        wb.close()
 
-    wb.save(path)
-    wb.close()
+        # 500件チェック
+        if ttlcnt > maxcnt:
+            break
+
+        # 次ページ移行
+        nextbutton = driver.find_elements(By.CLASS_NAME, "css-13p07ha.e8ju0x50")
+        if len(nextbutton)>1:
+            nextbutton[1].click()
+        else:
+            nextbutton[0].click()
+        time.sleep(5)
 
     # クローズ処理
-    time.sleep(10)
+    time.sleep(5)
     driver.close()
     driver.quit()
 
